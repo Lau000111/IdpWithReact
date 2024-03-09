723a5e95d5d323b3fa0603239bdf99f44d934b67
Lau000111 <lau000111.ll@gmail.com>
2024-03-09 21:51:14 +0100

    initial
    initial

---
+++ React.Bff/Program.cs
@@ -0,0 +1,64 @@
+using Duende.Bff.Yarp;
+using React.Bff;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddBff()
+    .AddRemoteApis();
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultScheme = "cookie";
+    options.DefaultChallengeScheme = "oidc";
+    options.DefaultSignOutScheme = "oidc";
+}).AddCookie("cookie", options =>
+{
+    options.Cookie.Name = "__Host-bff";
+    options.Cookie.SameSite = SameSiteMode.Strict;
+}).AddOpenIdConnect("oidc", options =>
+{
+    options.Authority = "https://localhost:5001/";
+    options.ClientId = "interactive.confidential";
+    options.ClientSecret = "secret";
+    options.ResponseType = "code";
+    options.ResponseMode = "query";
+
+    options.GetClaimsFromUserInfoEndpoint = true;
+    options.MapInboundClaims = false;
+    options.SaveTokens = true;
+
+    options.Scope.Clear();
+    options.Scope.Add("openid");
+    options.Scope.Add("profile");
+    options.Scope.Add("api1");
+    options.Scope.Add("offline_access");
+
+    options.TokenValidationParameters = new()
+    {
+        NameClaimType = "name",
+        RoleClaimType = "role"
+    };
+});
+
+builder.Services.AddAuthorization();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+
+app.UseHttpsRedirection();
+app.UseAuthentication();
+app.UseBff();
+app.UseAuthorization();
+app.MapBffManagementEndpoints();
+
+app.MapGroup("/todos")
+    .ToDoGroup()
+    .RequireAuthorization()
+    .AsBffApiEndpoint();
+
+app.MapFallbackToFile("/index.html");
+
+app.Run();
